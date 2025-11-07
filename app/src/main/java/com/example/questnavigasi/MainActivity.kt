1075c59ff881f57b1542052a23e379fc64b5f836
taznimt <taznimanwar06@gmail.com>
2025-11-07 20:03:52 +0700

    add MainActivity setup with HomeApp composable and Scaffold layout

--- app/src/main/java/com/example/questnavigasi/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasi/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestNavigasiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HomeApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
