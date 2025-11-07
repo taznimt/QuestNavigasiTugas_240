9bee07d3dff83c61d12104002457bc5ec303b71d
taznimt <taznimanwar06@gmail.com>
2025-11-07 19:39:27 +0700

    add HomeApp composable with navigation between Beranda, List, and Formulir

--- app/src/main/java/com/example/questnavigasi/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasi/Navigasi.kt
@@ -1,8 +1,60 @@
 package com.example.questnavigasi
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Beranda,
     Formulir,
     List
 }
+
+@Composable
+fun HomeApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Beranda.name,
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Beranda.name) {
+                Homepage(
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.List.name)
+                    }
+                )
+            }
+
+            composable(route = Navigasi.List.name) {
+                DaftarPeserta(
+                    onBackToBerandaClick = {
+                        cancelAndBackToBeranda(navController)
+                    },
+                    OnBtnToFormulirClick = {
+                        navController.navigate(Navigasi.Formulir.name)
+                    }
+                )
+            }
+
+            composable(route = Navigasi.Formulir.name) {
+                FormDataDiri(
+                    onBackToBtnClick = {
+                        cancelAndBackToList(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+
 
